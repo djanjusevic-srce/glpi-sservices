211bb8d6c899b8793ccb580b42b6bd64f99e76da
root <root@mon-test.srce.hr>
2026-05-17 00:47:26 +0200

    Fix version to INT(11) UNSIGNED

--- install/mysql/glpi_plugin_sservices_migrations.sql
+++ install/mysql/glpi_plugin_sservices_migrations.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS `glpi_plugin_sservices_migrations` (
-    `version` INT(11) NOT NULL,
+    `version` INT(11) UNSIGNED NOT NULL,
     KEY `version` (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
